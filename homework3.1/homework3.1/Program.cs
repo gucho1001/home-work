3363372de2dbf8ba3ca425782510819ef58fa86a
gucho1001 <maerotopin@gmail.com>
2025-05-08 20:45:45 +0300

    added a check

--- homework3.1/homework3.1/Program.cs
+++ homework3.1/homework3.1/Program.cs
@@ -4,16 +4,23 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter first boolean value");
-        bool firstBool = Convert.ToBoolean(Console.ReadLine());
-        Console.WriteLine("Enter second boolean value");
-        bool secondBool = Convert.ToBoolean(Console.ReadLine());
+        try
+        {
+            Console.WriteLine("Enter first boolean value");
+            bool firstBool = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Enter second boolean value");
+            bool secondBool = Convert.ToBoolean(Console.ReadLine());
 
-        Console.WriteLine($"B1\t|\tB2\t|\tB1 ^ B2\t|\tB1 && B2\t|\tB1 || B2\t|\t!B1");
-        Console.WriteLine(new string('_', 100));
-        Console.WriteLine($"\n{firstBool}\t|\t{secondBool}\t" +
-            $"|\t{firstBool ^ secondBool}\t|\t{firstBool && secondBool}\t\t" +
-            $"|\t{firstBool || secondBool}\t\t|\t{!firstBool}");
+            Console.WriteLine($"B1\t|\tB2\t|\tB1 ^ B2\t|\tB1 && B2\t|\tB1 || B2\t|\t!B1");
+            Console.WriteLine(new string('_', 100));
+            Console.WriteLine($"\n{firstBool}\t|\t{secondBool}\t" +
+                $"|\t{firstBool ^ secondBool}\t|\t{firstBool && secondBool}\t\t" +
+                $"|\t{firstBool || secondBool}\t\t|\t{!firstBool}");     
+        }
+        catch
+        {
+            Console.WriteLine("Error:\nEnter true or false!");
+        }
 
         Console.ReadKey();
     }
