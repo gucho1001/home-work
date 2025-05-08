785c3e5e99ec5fb9b90fae2745eed52a196481c6
gucho1001 <maerotopin@gmail.com>
2025-05-08 20:11:40 +0300

    HomeWork3.1

---
+++ homework3.1/homework3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace homework3_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first boolean value");
+        bool firstBool = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Enter second boolean value");
+        bool secondBool = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine($"B1\t|\tB2\t|\tB1 ^ B2\t|\tB1 && B2\t|\tB1 || B2\t|\t!B1");
+        Console.WriteLine(new string('_', 100));
+        Console.WriteLine($"\n{firstBool}\t|\t{secondBool}\t" +
+            $"|\t{firstBool ^ secondBool}\t|\t{firstBool && secondBool}\t\t" +
+            $"|\t{firstBool || secondBool}\t\t|\t{!firstBool}");
+
+        Console.ReadKey();
+    }
+}
