092eb24acb3547326105226d6a89ded33403841b
gucho1001 <maerotopin@gmail.com>
2025-05-11 14:37:41 +0300

    HomeWork4.0

---
+++ Homework4.0/Homework4.0/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Homework4._0;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter size of array (0-100)");
+        int input;
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                if (input >= 0 && input <= 100)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Enter size of array (0-100)");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: input number!");
+            }
+        }
+        int[] array = new int[input];
+        var count = 0;
+        Random rnd = new Random();
+
+        Console.WriteLine("\nCycle while:");
+        while (count < array.Length)
+        {
+            array[count] = rnd.Next(100);
+            Console.Write(array[count] + " ");
+            count++;
+        }
+
+        Console.WriteLine("\n\nCycle for with foreach:");
+        for (count = 0; count < array.Length; count++)
+        {
+            array[count] = rnd.Next(100);
+        }
+
+        foreach (int i in array)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\n\nCycle do while:");
+        count = 0;
+        if (array.Length > 0)
+        {
+            do
+            {
+                array[count] = rnd.Next(100);
+                Console.Write(array[count] + " ");
+                count++;
+            }
+            while (count < array.Length);
+        }
+        Console.ReadKey();
+    }
+}
