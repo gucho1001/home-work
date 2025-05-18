c3022b495d2c58c14bedc5b15fbf328cd4d9f2da
gucho1001 <maerotopin@gmail.com>
2025-05-18 16:43:12 +0300

    HomeWork5.0

---
+++ Homework5/Homework5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var bookOne = new Book
+        {
+            Title = "Harik v Pote",
+            Author = "Vitalya NKVD",
+            Pages = 228
+        };
+
+        var bookTwo = new Book("The Black Lord", "Tolik", 322);
+
+        Console.WriteLine(bookOne.GetDescription());
+        Console.WriteLine(bookTwo.GetDescription());
+    }
+}
